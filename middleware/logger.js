8994083d1af95acccad482f5fc382b49c9cd19fe
zziaho <rlawlgh1100@naver.com>
2025-06-26 14:09:31 +0900

    fix: log timestamp formmat 개선

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,6 +1,7 @@
 function logger(req, res, next)  {
-    const now = new Date().toISOString();
-    console.log(`[${now}] ${req.method} ${req.url}`);
+    const now = new Date();
+    const timestamp = new Date(now.getTime() + 9 * 60 * 60 * 1000).toISOString().replace('T', ' ').substring(0, 19);
+    console.log(`[${timestamp}] ${req.method} ${req.url}`);
     next();
 }
 
